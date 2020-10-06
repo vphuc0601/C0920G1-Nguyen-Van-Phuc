92047d75d70d58980f047364195a7eaeaa3a1eeb
vphuc0601 <nguyenvanphuc0601@gmail.com>
2020-10-06 15:10:17 +0700

    OOP

--- Modul1/JavaScript/Bài tập/furama.js
+++ Modul1/JavaScript/Bài tập/furama.js
@@ -1,7 +1,23 @@
-let Name=document.getElementById(name);
-let CMND=document.getElementById(CMND);
-function Confirm() {
-    document.getElementById(Name).innerHTML=name.value;
-    document.getElementById(CMND).innerHTML=CMND.value;
 
+function Confirm() {
+    let name=document.getElementById("name").value;
+    let cmnd=document.getElementById("cmnd").value;
+    let birthday=document.getElementById("birthday").value;
+    let mail=document.getElementById("mail").value;
+    let adress=document.getElementById("adress").value;
+    let custom=document.getElementById("custom").value;
+    let sever=document.getElementById("sever").value;
+    let type=document.getElementById("type").value;
+    let price=document.getElementById("price").value;
+    alert(
+        "Họ và tên:"+name+"\n"+
+        "CMND:"+cmnd+"\n"+
+        "Ngày sinh:"+birthday+"\n"+
+        "Email:"+mail+"\n"+
+        "Địa chỉ:"+adress+"\n"+
+        "Custom:"+custom+"\n"+
+        "Dịch vụ:"+sever+"\n"+
+        "Loại:"+type+"\n"+
+        "Bảng giá:"+price
+    );
 }
