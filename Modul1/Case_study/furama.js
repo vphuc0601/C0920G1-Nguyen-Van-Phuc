6b971cb5d37f6a96cf34d74766f1f7b49ec68fd4
vphuc0601 <nguyenvanphuc0601@gmail.com>
2020-10-19 13:38:42 +0700

    1

---
+++ Modul1/Case_study/furama.js
@@ -0,0 +1,29 @@
+function confirm(){
+    let name=document.getElementById("name").value;
+    let cmnd=document.getElementById("cmnd").value;
+    let birthday=document.getElementById("birthday").value;
+    let mail=document.getElementById("mail").value;
+    let address=document.getElementById('address').value;
+    let sever=document.getElementById('sever').value;
+    let custom=document.getElementById('custom').value;
+    let room=document.getElementById('room').value;
+    let discount=document.getElementById('discount').value;
+    let quanlity=document.getElementById('quanlity').value;
+    document.getElementById('a').innerHTML=name
+    document.getElementById('b').innerHTML=cmnd
+    document.getElementById('c').innerHTML=birthday
+    document.getElementById('d').innerHTML=mail
+    document.getElementById('e').innerHTML=address
+    document.getElementById('f').innerHTML=sever
+    document.getElementById('h').innerHTML=custom
+    document.getElementById('g').innerHTML=room
+    document.getElementById('k').innerHTML=discount
+    document.getElementById('l').innerHTML=quanlity
+
+
+
+
+
+
+}
+
