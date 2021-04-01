172e18c83e19789a81a4615a08301fe688c24794
vphuc0601 <nguyenvanphuc0601@gmail.com>
2021-04-01 22:54:23 +0700

    fix

---
+++ Modul4/03_Views_Thymeleaf/Bai_tap/Product_Manage/src/main/java/model/Product.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Product {
+    int id;
+    String name;
+    double price;
+    String status;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, double price, String status) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
